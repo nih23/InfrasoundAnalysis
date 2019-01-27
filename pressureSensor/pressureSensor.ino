c7da31e27c67b4dd4fc50db48f8dd02aab87453f
nih23 <nico.hoffmann@tu-dresden.de>
2019-01-27 12:23:26 -0500

    Increased sampling rate of the sensor by deactivating integrating of last n measurements

--- pressureSensor/pressureSensor.ino
+++ pressureSensor/pressureSensor.ino
@@ -5,19 +5,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float IN1 = analogRead(A0);
-  delay(10);
+  float IN = analogRead(A0);
+  /*delay(10);
   float IN2 = analogRead(A0);
   delay(10);
   float IN3 = analogRead(A0);
   delay(10);
   float IN4 = analogRead(A0);
-  float IN = (IN1+IN2+IN3+IN4) / 4.;
+  float IN = (IN1+IN2+IN3+IN4) / 4.; */
   
   float pressure = ((IN/1023.0 - .2) / .2); //transfer function from manufacturer
   byte * b = (byte *) &pressure;
   
   Serial.print(pressure,5);
   Serial.println();
-  delay(100);
+  //delay(100);
 }
